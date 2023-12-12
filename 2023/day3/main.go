13dd5a724e04670fe9ed43204a6bfbfe3605fa08
Filip Weidemann <filip.weidemann@outlook.de>
2023-12-12 22:44:44 +0100

    2023: day3 part 1, reworked

--- 2023/day3/main.go
+++ 2023/day3/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"github.com/filipweidemann/advent-of-code/utils"
 	"strconv"
 	"strings"
 	"unicode"
-
-	"github.com/filipweidemann/advent-of-code/utils"
 )
 
 //go:embed input-test.txt
@@ -18,23 +17,66 @@ var testInputLines []string
 var input string
 var inputLines []string
 
+func init() {
+	testInputLines = strings.Split(testInput, "\n")
+	testInputLines = testInputLines[:len(testInputLines)-1]
+	inputLines = strings.Split(input, "\n")
+	inputLines = inputLines[:len(inputLines)-1]
+}
+
 type Symbol struct {
 	LineID int
 	Index  int
 }
 
-type ParsedLine struct {
+type Digit struct {
 	LineID  int
-	Symbols []Symbol
+	Indices []int
+	Runes   []rune
+	Value   int
 }
 
-func init() {
-	testInputLines = strings.Split(testInput, "\n")
-	inputLines = strings.Split(input, "\n")
+func (d *Digit) HasAdjacentSymbol(l []Line) bool {
+	// we assume equally long lines, so just grab one and get their length
+	lineLength := len(l[0].Content)
+
+	// vertical indices
+	startLine := utils.MinWithBound(d.LineID-1, 0)
+	endLine := utils.MaxWithBound(d.LineID+1, lineLength-1)
+
+	// horizontal indices
+	startIndex := utils.MinWithBound(d.Indices[0], 0)
+	endIndex := utils.MaxWithBound(d.Indices[len(d.Indices)-1], lineLength)
+
+	for i := startLine; i <= endLine; i++ {
+		for j := startIndex; j <= endIndex; j++ {
+			c := l[i].Content[j]
+			if string(c) != "." && !unicode.IsDigit(rune(c)) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+type Line struct {
+	LineID  int
+	Content string
+}
+
+func LinesFromSlice(s []string) []Line {
+	lines := []Line{}
+
+	for i, l := range s {
+		lines = append(lines, Line{i, l})
+	}
+
+	return lines
 }
 
 func main() {
-	sum := Part1(inputLines)
+	sum := Part1(testInputLines)
 	fmt.Printf("Part 1: %v \n", sum)
 
 	power := Part2(inputLines)
@@ -42,21 +84,18 @@ func main() {
 }
 
 func Part1(i []string) int {
-	symbols := make([]Symbol, 0)
+	// var symbols []Symbol
+	var digits []Digit
 
-	for ind, l := range i {
-		lineSymbols := ParseLine(l, ind)
-		for _, ls := range lineSymbols {
-			symbols = append(symbols, ls)
-		}
-	}
+	lines := LinesFromSlice(i)
+	_, digits = Collect(lines)
 
 	sum := 0
-	for symbolIndex, symbol := range symbols {
-		fmt.Printf("Symbol: %v --- ", symbol)
-		fmt.Printf("Running for symbol #%v \n", symbolIndex)
-
-		sum += GetAdjacentNumbersSum(symbol, &i)
+	for _, d := range digits {
+		s := d.HasAdjacentSymbol(lines)
+		if s {
+			sum += d.Value
+		}
 	}
 
 	return sum
@@ -66,101 +105,45 @@ func Part2(i []string) uint64 {
 	return 0
 }
 
-func GetAdjacentNumbersSum(s Symbol, l *[]string) int {
-	startLine := utils.MinWithBound(s.LineID-1, 0)
-	endLine := utils.MaxWithBound(s.LineID+1, len((*l))-1)
-	startIndex := utils.MinWithBound(int(s.Index), 0)
-	endIndex := utils.MaxWithBound(int(s.Index)+1, len((*l)[0])-1)
+func Collect(l []Line) ([]Symbol, []Digit) {
+	var symbols []Symbol
+	var digits []Digit
 
-	digits := make([]int, 0)
-	il := len((*l))
+	for lineIndex, line := range l {
+		digit := Digit{LineID: lineIndex}
 
-	for i := startLine; i <= endLine; i++ {
-		if i == il {
-			break
-		}
-
-		lineDigits := ""
-		ll := len((*l)[0])
-		for j := startIndex; j <= endIndex; j++ {
-			if j == ll || len((*l)[i]) == 0 {
-				break
+		for runeIndex, rune := range line.Content {
+			if unicode.IsDigit(rune) {
+				digit.Runes = append(digit.Runes, rune)
+				digit.Indices = append(digit.Indices, runeIndex)
+				continue
 			}
 
-			if c := (*l)[i][j]; unicode.IsDigit(rune(c)) {
-				lineDigits = lineDigits + string(c)
-			} else {
-				lineDigits = lineDigits + " "
+			// Now, also check for symbols
+			if rune != '.' {
+				symbols = append(symbols, Symbol{lineIndex, runeIndex})
 			}
-		}
 
-		// No digit in string
-		if strings.TrimSpace(lineDigits) == "" {
-			continue
-		}
-
-		// If left side contains a digit, scan further to the left
-		if !strings.HasPrefix(lineDigits, " ") {
-			leftExtension := ""
-			for li := startIndex - 1; li >= 0; li-- {
-				if c := (*l)[i][li]; unicode.IsDigit(rune(c)) {
-					leftExtension = string(c) + leftExtension
-				} else {
-					break
-				}
+			// If no prior digit is written, just go to next char
+			if len(digit.Runes) == 0 {
+				continue
 			}
-			lineDigits = leftExtension + lineDigits
-		}
 
-		// If right side contains a digit, scan further to the left
-		if !strings.HasSuffix(lineDigits, " ") {
-			rightExtension := ""
-			for ri := endIndex + 1; ri < len(*l); ri++ {
-				if ri == ll {
-					break
-				}
-				if c := (*l)[i][ri]; unicode.IsDigit(rune(c)) {
-					rightExtension = string(c) + rightExtension
-				} else {
-					break
-				}
+			// If prior digit was written, determine its value and append it
+			fmt.Println("Converting runes to Str: ", string(digit.Runes))
+			value, err := strconv.Atoi(string(digit.Runes))
+			if err != nil {
+				panic("Could not parse runes to digit")
 			}
-			lineDigits = lineDigits + rightExtension
-		}
 
-		// Line is completely evaluated, convert the digits to actual uints
-		ts := strings.TrimSpace(lineDigits)
-		for _, d := range strings.Split(ts, " ") {
-			pd, err := strconv.Atoi(d)
-			if err == nil {
-				digits = append(digits, pd)
-			}
+			digit.Value += value
+			fmt.Println("Appending Digit: ", digit)
+			digits = append(digits, digit)
+			digit.Runes = nil
+			digit.Indices = nil
+
 		}
 	}
 
-	sum := 0
-	for _, val := range digits {
-		sum += val
-	}
-
-	return sum
-}
-
-func ParseLine(l string, ind int) []Symbol {
-	symbols := make([]Symbol, 0)
-
-	for i, r := range l {
-		if unicode.IsDigit(r) || r == '.' {
-			continue
-		}
-
-		symbol := Symbol{
-			LineID: ind,
-			Index:  i,
-		}
-
-		symbols = append(symbols, symbol)
-	}
-
-	return symbols
+	return symbols, digits
 }
